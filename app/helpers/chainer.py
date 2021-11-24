e556e6a1ed2b57c6603a3e38d812b77269d23686
Evgenii Titov <1994etitov@gmail.com>
2021-11-24 19:40:23 +1100

    Fixed chainer bug

--- app/helpers/chainer.py
+++ app/helpers/chainer.py
@@ -4,7 +4,6 @@ import typing as t
 class Chainer:
     """
     Calls provided functions in a sequence while passing results between them
-    # TODO: Consider reporting when a function returns None
     """
 
     def __init__(
@@ -25,27 +24,44 @@ class Chainer:
 
     def __call__(self, *args, **kwargs) -> t.Any:
         res = self._first(*args, **kwargs)
+        if res is None:
+            raise Exception("The first step did not return any results")
         if self._verbose:
             print(res)
-        for i in range(len(self._remaining)):
+
+        n_remaining = len(self._remaining)
+        for i in range(n_remaining):
             func = self._remaining[i]
             res = func(res)
+            if res is None and i != n_remaining - 1:
+                raise Exception(
+                    f"Intermediate step {func} did not " f"return any result"
+                )
             if self._verbose:
                 print(res)
         return res
 
 
 if __name__ == "__main__":
-    f1 = lambda number: number * 2
-    f2 = lambda number: number + 10
+    # f1 = lambda number: number * 2
+    # f2 = lambda number: number + 10
+    #
+    # def f3(number):
+    #     return number / 100
+    #
+    # class F4:
+    #     def __call__(self, number, *args, **kwargs):
+    #         return number % 2 == 0
 
-    def f3(number):
-        return number / 100
+    class F1:
+        def __call__(self, value_in, *args, **kwargs):
+            return value_in * 2
 
-    class F4:
-        def __call__(self, number, *args, **kwargs):
-            return number % 2 == 0
+    class F2:
+        def __call__(self, value_in, *args, **kwargs):
+            return value_in - 100
 
-    chain = Chainer(functions=[f1, f2, f3, F4()], verbose=True)
+    chain = Chainer(functions=[F1(), F2()], verbose=True)
     result = chain(100)
-    print(result)
+
+    print("\nFinal result:", result)
